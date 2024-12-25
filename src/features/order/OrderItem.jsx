7b967867b9888ec543a4209eb92106afbea16acc
bishoy labib <bishoylabib78@gmail.com>
2024-12-25 20:04:23 +0200

    OrderItem.jsx

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,16 +1,23 @@
-function OrderItem({ item, isLoadingIngredients, ingredients }) {
-  const { quantity, name, totalPrice } = item;
+import { formatCurrency } from '../../utils/helpers'
 
-  return (
-    <li>
-      <div>
-        <p>
-          <span>{quantity}&times;</span> {name}
-        </p>
-        <p>{formatCurrency(totalPrice)}</p>
-      </div>
-    </li>
-  );
+function OrderItem({ item, isLoadingIngredients, ingredients }) {
+    const { quantity, name, totalPrice } = item
+
+    return (
+        <li>
+            <div>
+                <p>
+                    <span>{quantity}&times;</span> {name}
+                </p>
+                <p>{formatCurrency(totalPrice)}</p>
+                <p className="text-sm capitalize italic text-stone-500">
+                    {isLoadingIngredients
+                        ? 'Loading...'
+                        : ingredients.join(', ')}
+                </p>
+            </div>
+        </li>
+    )
 }
 
-export default OrderItem;
+export default OrderItem
