60185085a35f885b66ebcba28ba822d3f0cb46c3
bishoy labib <bishoylabib78@gmail.com>
2024-12-25 20:04:11 +0200

    Order.jsx

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,59 +1,88 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
-import { getOrder } from "../../services/apiRestaurant";
+import { useFetcher, useLoaderData } from 'react-router-dom'
+import { getOrder } from '../../services/apiRestaurant'
 import {
-  calcMinutesLeft,
-  formatCurrency,
-  formatDate,
-} from "../../utils/helpers";
+    calcMinutesLeft,
+    formatCurrency,
+    formatDate,
+} from '../../utils/helpers'
+import { useEffect } from 'react'
+import OrderItem from './OrderItem'
 
 function Order() {
-  const order = useLoaderData();
+    const order = useLoaderData()
 
-  // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
-  const {
-    id,
-    status,
-    priority,
-    priorityPrice,
-    orderPrice,
-    estimatedDelivery,
-    cart,
-  } = order;
-  const deliveryIn = calcMinutesLeft(estimatedDelivery);
+    const fetcher = useFetcher()
 
-  return (
-    <div>
-      <div>
-        <h2>Status</h2>
+    useEffect(
+        function () {
+            if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu')
+        },
+        [fetcher]
+    )
 
+    // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
+    const {
+        id,
+        status,
+        priority,
+        priorityPrice,
+        orderPrice,
+        estimatedDelivery,
+        cart,
+    } = order
+    const deliveryIn = calcMinutesLeft(estimatedDelivery)
+
+    return (
         <div>
-          {priority && <span>Priority</span>}
-          <span>{status} order</span>
+            <div>
+                <h2>Status</h2>
+
+                <div>
+                    {priority && <span>Priority</span>}
+                    <span>{status} order</span>
+                </div>
+            </div>
+
+            <div>
+                <p>
+                    {deliveryIn >= 0
+                        ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
+                        : 'Order should have arrived'}
+                </p>
+                <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+            </div>
+            <ul>
+                {cart.map((item) => (
+                    <OrderItem
+                        item={item}
+                        key={item.pizzaId}
+                        isLoadingIngredients={fetcher.state === 'loading'}
+                        ingredients={
+                            fetcher?.data?.find((el) => el.id === item.pizzaId)
+                                .ingredients ?? []
+                        }
+                    />
+                ))}
+            </ul>
+            <div>
+                <p>Price pizza: {formatCurrency(orderPrice)}</p>
+                {priority && (
+                    <p>Price priority: {formatCurrency(priorityPrice)}</p>
+                )}
+                <p>
+                    To pay on delivery:{' '}
+                    {formatCurrency(orderPrice + priorityPrice)}
+                </p>
+            </div>
         </div>
-      </div>
-
-      <div>
-        <p>
-          {deliveryIn >= 0
-            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
-            : "Order should have arrived"}
-        </p>
-        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
-      </div>
-
-      <div>
-        <p>Price pizza: {formatCurrency(orderPrice)}</p>
-        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
-      </div>
-    </div>
-  );
+    )
 }
+3
 export async function loader({ params }) {
-  const order = await getOrder(params.orderId);
-  return order;
+    const order = await getOrder(params.orderId)
+    return order
 }
 
-export default Order;
+export default Order
