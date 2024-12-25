610da4629a64b8abab80746928f0c2c3b602341d
bishoy labib <bishoylabib78@gmail.com>
2024-12-25 12:45:19 +0200

    CartItem.jsx

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,4 +1,6 @@
 import { formatCurrency } from '../../utils/helpers'
+import DeleteItem from './DeleteItem'
+import UpdateItemQuantity from './UpdateItemQuantity'
 function CartItem({ item }) {
     const { pizzaId, name, quantity, totalPrice } = item
 
@@ -9,6 +11,9 @@ function CartItem({ item }) {
             </p>
             <div>
                 <p>{formatCurrency(totalPrice)}</p>
+
+                <UpdateItemQuantity pizzaId={pizzaId} quantity={quantity} />
+                <DeleteItem itemId={pizzaId} />
             </div>
         </li>
     )
