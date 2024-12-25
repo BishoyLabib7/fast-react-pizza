fdca70b8ade5025dc545056aa94605cfdd7a336d
bishoy labib <bishoylabib78@gmail.com>
2024-12-25 12:46:41 +0200

    SearchOrder.jsx

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,26 +1,27 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function SearchOrder() {
-  const [quary, setQuary] = useState();
-  const navigate = useNavigate();
+    const [quary, setQuary] = useState()
+    const navigate = useNavigate()
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (!quary) return;
-    navigate(`/order/${quary}`);
-    setQuary("");
-  }
+    function handleSubmit(e) {
+        e.preventDefault()
+        if (!quary) return
+        navigate(`/order/${quary}`)
+        setQuary('')
+    }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder="search order #"
-        value={quary}
-        onChange={(e) => setQuary(e.target.value)}
-      />
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                placeholder="Search order #"
+                value={quary}
+                onChange={(e) => setQuary(e.target.value)}
+                className="rounded-full px-4 py-2 text-sm bg-yellow-100 placeholder:text-stone-400 w-28 sm:w-64 sm:focus:w-72 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50"
+            />
+        </form>
+    )
 }
 
-export default SearchOrder;
+export default SearchOrder
