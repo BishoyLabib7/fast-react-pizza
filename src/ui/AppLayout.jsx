018a60129b0d49a5330b08dafa4b6b4f9e092222
bishoy labib <bishoylabib78@gmail.com>
2024-12-25 15:36:12 +0200

    AppLayout.jsx

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -2,10 +2,13 @@ import { Outlet, useNavigation } from 'react-router-dom'
 import CartOverview from '../features/cart/CartOverview'
 import Header from './Header'
 import Loader from './Loader'
+import { useSelector } from 'react-redux'
+import { getCart } from '../features/cart/cartSlice'
 
 function AppLayout() {
     const navigation = useNavigation()
     const isLoading = navigation.state === 'loading'
+    const isEmpty = useSelector(getCart).length > 0
 
     return (
         <div className="grid h-screen grid-rows-[auto_1fr_auto]">
@@ -18,7 +21,7 @@ function AppLayout() {
                 </main>
             </div>
 
-            <CartOverview />
+            {isEmpty && <CartOverview />}
         </div>
     )
 }
